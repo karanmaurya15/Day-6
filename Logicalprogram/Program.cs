a29932ad5f9e8940f492abec60fe43cc1233593d
karanmaurya15 <karanmaurya9663@gmail.com>
2022-10-17 15:34:05 +0530

    program  basic

---
+++ Logicalprogram/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Logicalprogram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
