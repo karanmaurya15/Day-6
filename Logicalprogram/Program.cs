acba5f182ba3c629c302dc535367496cd4dcf5ea
karanmaurya15 <karanmaurya9663@gmail.com>
2022-10-17 15:45:31 +0530

    program  added

--- Logicalprogram/Program.cs
+++ Logicalprogram/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter the Number");
+            double num = Convert.ToDouble(Console.ReadLine());
+            int sum = 0;
+            Console.WriteLine("Series are as follows");
+            for (int i = 0; i <= num; i++)
+            {
+                int[] number = { i };
+                sum += i;
+                Console.WriteLine(sum);
+            }
         }
     }
 }
